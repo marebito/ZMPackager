349618fb5d8596e1d65848d07092d228ac284f62
༺ Yuri ༒ Boyka ༻ <lilung.chao@gmail.com>
2019-01-11 17:43:09 +0800

    add third party dependency

--- ZMPackager/main.m
+++ ZMPackager/main.m
@@ -9,9 +9,11 @@
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char* argv[]) { return NSApplicationMain(argc, argv); }
+
 /**********************************TODO**********************************/
 /* 1. 项目国际化支持导入csv/xls/                                         */
 /* Plist编辑器: https://github.com/AlonsoZhang/PlistEditor.git          */
+/* Plist编辑: https://github.com/LeoNatan/LNPropertyListEditor.git      */
 /* 读取Excel https://github.com/dhoerl/DHlibxls                         */
 /* 写入Excel https://github.com/tucan9389/excel-example-ios.git         */
 /* 写入XLS/XLSX https://github.com/lrossi/libxlsxwriterCocoaExamples.git*/
@@ -21,7 +23,22 @@ int main(int argc, const char* argv[]) { return NSApplicationMain(argc, argv); }
 /* 2. 启动图及图标自动适配处理(AppIcon.sh LaunchImage.sh)                 */
 /* 3. 自动化打包                                                         */
 /* 4. 推送测试工具                                                       */
-/* 5. 正则匹配工具（功能参考Expressions应用)                              */
-/* 6. 网络请求(SOAP/RESTFUL)                                            */
+/* 5. 正则匹配工具（功能参考Expressions应用, TextLab)                     */
+/* 6. 网络请求(SOAP/RESTFUL) (功能参考Paw)                               */
 /* 7. 导出数据模型(支持WCDB)                                             */
+/* 8. 重签名 https://github.com/LigeiaRowena/Resign.git                 */
+/* MobileProfile: https://github.com/LigeiaRowena/ProvisioningInfo.git */
+/* MobileProfile: https://github.com/jimmya/YAProvisioningProfile.git  */
+/* MenuBar: https://github.com/phranck/CCNStatusItem.git               */
+/* Mac硬件信息: https://github.com/perfaram/PFSystemKit.git             */
+/* 滚动文本视图: https://github.com/orklann/ScrollingTextView.git       */
+/* 应用设置视图: https://github.com/tristanseifert/TSAppKit.git         */
+/* DarkMode视图: https://github.com/weAreYeah/WAYTheDarkSide.git       */
+/* 问题搜索视图: https://github.com/weAreYeah/WAYSourceListWindow       */
+/* 移除图片Alpha通道: https://github.com/bpolat/Alpha-Channel-Remover.git */
+/* 打开文件: https://github.com/Kito0615/LOpenPanelAccessoryView.git    */
+/* UI辅助框架: https://github.com/HsiangHo/XUIKit.git                   */
+/* FlatButton: https://github.com/Sunnyyoung/SYFlatButton.git           */
+/* AppIconMaker: https://github.com/Kito0615/AppIconAutoMaker.git       */
+/* ComboBox: https://github.com/lemonmojo/LMComboBox.git                */
 /************************************************************************/
