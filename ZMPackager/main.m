1d69180270134040e8c486b15d80ef8917b42707
༺ Yuri ༒ Boyka ༻ <lilung.chao@gmail.com>
2019-01-12 17:52:57 +0800

    add third party integrate plan

--- ZMPackager/main.m
+++ ZMPackager/main.m
@@ -21,7 +21,9 @@ int main(int argc, const char* argv[]) { return NSApplicationMain(argc, argv); }
 /* XML映射Object https://github.com/uacaps/NSObject-ObjectMap.git       */
 /* CSVExport: https://github.com/vigneshuvi/SwiftCSVExport.git          */
 /* 2. 启动图及图标自动适配处理(AppIcon.sh LaunchImage.sh)                 */
+/* 图片导入 https://github.com/angelvasa/AVXCAssets-Generator.git       */
 /* 3. 自动化打包                                                         */
+/* IPAExporter: https://github.com/tue-savvy/IPAExporter.git            */
 /* 4. 推送测试工具                                                       */
 /* 5. 正则匹配工具（功能参考Expressions应用, TextLab)                     */
 /* 6. 网络请求(SOAP/RESTFUL) (功能参考Paw)                               */
@@ -34,11 +36,25 @@ int main(int argc, const char* argv[]) { return NSApplicationMain(argc, argv); }
 /* 滚动文本视图: https://github.com/orklann/ScrollingTextView.git       */
 /* 应用设置视图: https://github.com/tristanseifert/TSAppKit.git         */
 /* DarkMode视图: https://github.com/weAreYeah/WAYTheDarkSide.git       */
-/* 问题搜索视图: https://github.com/weAreYeah/WAYSourceListWindow       */
+/* 问题搜索视图: https://github.com/weAreYeah/WAYSourceListWindow.git   */
 /* 移除图片Alpha通道: https://github.com/bpolat/Alpha-Channel-Remover.git */
 /* 打开文件: https://github.com/Kito0615/LOpenPanelAccessoryView.git    */
 /* UI辅助框架: https://github.com/HsiangHo/XUIKit.git                   */
 /* FlatButton: https://github.com/Sunnyyoung/SYFlatButton.git           */
+/* FlatButton: https://github.com/OskarGroth/FlatButton.git             */
 /* AppIconMaker: https://github.com/Kito0615/AppIconAutoMaker.git       */
 /* ComboBox: https://github.com/lemonmojo/LMComboBox.git                */
+/* 引导: https://github.com/chriszielinski/Enlighten.git                */
+/* 权限申请: https://github.com/MacPaw/PermissionsKit.git               */
+/* 日期控件: https://github.com/thierryH91200/THCalendar.git            */
+/* PageControl: https://github.com/c-Viorel/MBPageControl.git          */
+/* 加载动画: https://github.com/Henryforce/KRActivityIndicatorView.git  */
+/* TabView: https://github.com/Dis3buted/MyTabbedView.git              */
+/* JSON2Model: https://github.com/insanoid/SwiftyJSONAccelerator.git   */
+/* AddFileLicense: https://github.com/xxhp/LicenseNotice_XcodePlugin.git*/
+/* MordenLook: https://github.com/gyetvan-andras/ModernLook-OSX.git    */
+/* ChromeTab: https://github.com/onekiloparsec/KPCTabsControl.git      */
+/* 模拟器跳转: https://github.com/tue-savvy/SimulatorManager.git        */
+/* 平面Switch视图: https://github.com/iluuu1994/ITSwitch.git            */
+/* 进度条: https://github.com/insidegui/GRProgressIndicator.git        */
 /************************************************************************/
