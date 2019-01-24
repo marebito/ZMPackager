6a7bb445b67c6778cd44573af2453c169f77be18
༺ Yuri ༒ Boyka ༻ <lilung.chao@gmail.com>
2019-01-24 17:29:15 +0800

    add multilineTextView

--- ZMPackager/Vendors/IPA/Public/NSDate+Relative.h
+++ ZMPackager/Vendors/IPA/Public/NSDate+Relative.h
@@ -1,11 +1,3 @@
-//
-//  DateHelper.h
-//  IPAExporter
-//
-//  Created by Tue Nguyen on 10/11/14.
-//  Copyright (c) 2014 HOME. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface NSDate(Relative)
--- ZMPackager/Vendors/IPA/Public/NSDate+Relative.m
+++ ZMPackager/Vendors/IPA/Public/NSDate+Relative.m
@@ -1,11 +1,3 @@
-//
-//  DateHelper.m
-//  IPAExporter
-//
-//  Created by Tue Nguyen on 10/11/14.
-//  Copyright (c) 2014 HOME. All rights reserved.
-//
-
 #import "NSDate+Relative.h"
 
 @implementation NSDate(Relative)
